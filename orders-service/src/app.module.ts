e14b54aa3307fe273d687876c265ec507cf2c20d
Fabricioanchundia <alex.anchundia3n@gmail.com>
2026-01-15 22:52:28 -0500

    Token protegido y ordenes hecho creadas por el api-gateway

--- orders-service/src/app.module.ts
+++ orders-service/src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { OrdersController } from './orders-service/orders.controller';
+import { OrdersService } from './orders-service/orders.service';
+import { Order } from './orders-service/entidad/order.entity';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Order],
+      synchronize: true, // ⚠️ solo dev
+    }),
+    TypeOrmModule.forFeature([Order]),
+  ],
   controllers: [OrdersController],
+  providers: [OrdersService],
 })
 export class AppModule {}
--- orders-service/src/orders-service/orders.controller.ts
+++ orders-service/src/orders-service/orders.controller.ts
@@ -1,22 +1,25 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post } from '@nestjs/common';
+import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  createOrder(
+  async createOrder(
     @Headers('x-user-id') userId: string,
     @Body() body: any,
   ) {
-    console.log('USER ID RECIBIDO:', userId);
+    const order = await this.ordersService.create(userId, body);
 
     return {
       message: 'Order created',
-      userId,
-      data: {
-        product: body.product ?? 'Laptop',
-        price: body.price ?? 1200,
-      },
+      data: order,
     };
+  }
+
+  @Get()
+  async listOrders(@Headers('x-user-id') userId: string) {
+    return this.ordersService.findByUser(userId);
   }
 }
